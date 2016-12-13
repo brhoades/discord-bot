39aabcdb85fb31ecb4867d2b33e7752167186922
Billy Rhoades <aaron5367@gmail.com>
2016-12-12 20:25:26 -0600

    Giphy now handles now results gracefully.

--- modules/giphy.rb
+++ modules/giphy.rb
@@ -9,7 +9,7 @@ class GiphyFeature < BotFeature
   def register_handlers(bot, scheduler)
     bot.message(contains: /^[!\/]giphy/) do |event|
       $ignore << event.message.id
-      response = get_random_url(event.message, event.message.author.username)
+      response = get_random_url(event.message, event.message.author.username, event.channel.name =~ /giphy_nsfw/)
       author = event.message.author.username
       $giphys[author] = {
         message: event.respond(response),
@@ -26,24 +26,35 @@ class GiphyFeature < BotFeature
       if $giphys.has_key? author
         response = get_random_url($giphys[author][:original], author)
         $giphys[author][:message].delete
-        $giphys[author][:message] = event.respond(get_random_url($giphys[author][:original], author))
+        $giphys[author][:message] = event.respond(get_random_url($giphys[author][:original], author, event.channel.name =~ /giphy_nsfw/))
       end
     end
   end
 
   private
 
-  def get_random_url(event_message, author)
+  def get_random_url(event_message, author, nsfw=false)
     original = event_message.to_s
     original.gsub! /\&/, ""
     message = original.split(/ /)
     message.delete_at 0
 
-    url = URI("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg-13&tag=#{message.join("+")}")
+    url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=#{message.join("+")}"
+    if not nsfw
+      url += "&rating=pg-13"
+    end 
+
+    url = URI(url)
     response = JSON.parse(Net::HTTP.get(url))
-    if response["data"].has_key?('url')
-      url = response["data"]["url"]
-      "#{author}: #{original}\n#{url}"
+
+
+    if response["data"] and response["data"].is_a? Hash
+      if response["data"].has_key?('url')
+        url = response["data"]["url"]
+        "#{author}: #{original}\n#{url}"
+      end
+    else
+      "#{author}: #{original} (**no matches**)"
     end
   end
 end
