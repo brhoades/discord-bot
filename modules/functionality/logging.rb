af09d5cc8ad21bc89c5d2e7a0bd593ff1e32df6e
Billy Rhoades <aaron5367@gmail.com>
2016-12-30 12:53:52 -0600

    Logging to a database now works.

--- modules/functionality/logging.rb
+++ modules/functionality/logging.rb
@@ -8,33 +8,40 @@ module ModelHandlers
   # Given a Discordrb::Message, give a model instance of a Message.
   # Creates if necessary.
   def get_message(bot, discord_message, server=nil)
-    bot.db[:users].find
-    message = bot.db[:messages].where(discord_id: discord_message.id).all
-    puts "res: \"#{message}\""
-    if message.size == 0
+    message = bot.db[:messages].where(discord_id: discord_message.id).first
+
+    if not message
       user = get_user bot, discord_message.author
       server = get_server bot, server
       channel = get_channel bot, discord_message.channel
-    
-      message = bot.db[:messages].insert(user_id: user, channel_id: channel,
-                                         server_id: server,
+        puts %{
+User: "#{user}"
+Server: "#{server}"
+Channel: "#{channel}"
+}
+      message = bot.db[:messages].insert(user_id: user[:id], channel_id: channel[:id],
+                                         server_id: server[:id],
                                          timestamp: Time.now,
                                          message: discord_message.to_s)
+      bot.db[:messages].where(id: message).first
     end
-
-    message
   end
 
   # Given a Discordrb::User, give a model instance of a User.
   # Creates if necessary.
   def get_user(bot, discord_user)
-    user = bot.db[:users].where(discord_id: discord_user.id).all
+    user = bot.db[:users].where(discord_id: discord_user.id).first
 
-    if user.size == 0 
-      user = bot.db[:users].insert(discord_id: discord_user.id)
+    if not user
+      user_id = bot.db[:users].insert(discord_id: discord_user.id)
+      user = bot.db[:users].where(id: user_id).all
     end
 
-    user
+    if user.is_a? Array
+      user.first
+    else
+      user
+    end
   end
 
   # Given a Discordrb::Server, give a model instance of a server.
@@ -42,23 +49,33 @@ module ModelHandlers
   def get_server(bot, discord_server)
     server = bot.db[:servers].where(discord_id: discord_server.id).all
 
-    if server.size == 0
-      server = bot.db[:servers].insert(discord_id: discord_server.id)
+    if not server
+      server_id = bot.db[:servers].insert(discord_id: discord_server.id)
+      server = bot.db[:servers].where(id: server_id).all
     end
 
-    server
+    if server.is_a? Array
+      server.first
+    else
+      server
+    end
   end
 
   # Given a Discordrb::Channel, give a model instance of a channel.
   # Creates if necessary.
   def get_channel(bot, discord_channel)
-    channel = bot.db[:channels].where(discord_id: discord_channel.id).all
+    channel = bot.db[:channels].where(discord_id: discord_channel.id).first
 
-    if channel.size == 0
-      channel = bot.db[:channels].insert(discord_id: discord_channel.id)
+    if not channel
+      channel_id = bot.db[:channels].insert(discord_id: discord_channel.id)
+      channel = bot.db[:channels].where(id: channel_id).all
     end
 
-    channel
+    if channel.is_a? Array
+      channel.first
+    else
+      channel
+    end
   end
 end
 
@@ -89,7 +106,7 @@ class LoggingFeature < BotFeature
     end
 
     bot.message do |event|
-      # get_message(bot, event.message, event.server)
+      get_message(bot, event.message, event.server)
       $messages[event.message.id] = {
         user: event.message.author.username.to_s,
         timestamp: event.timestamp
@@ -99,9 +116,5 @@ class LoggingFeature < BotFeature
         $messages[event.message.id][:content] = event.message.content.to_s
       end
     end
-  end
-
-  def self.descendants
-    ObjectSpace.each_object(Class).select { |klass| klass < self }
   end
 end
